f7eb837b57f4dc648a866cd7cc2aee21c4c6c816
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2018-01-03 21:52:30 -0200

    Created and Updated file  - implements filter

--- _app/controllers/post.js
+++ _app/controllers/post.js
@@ -1,5 +1,6 @@
 angular.module('blog').controller('PostController', function($scope, $http){
 	$scope.posts = [];
+	$scope.bfilter = '';
 	var url = 'http://elsweb.servehttp.com:3030/post/consulta';
     $http.get(url)
     .success(function(posts){
