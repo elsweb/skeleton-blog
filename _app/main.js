689241daf694b3724b510b4f726f47cca8171859
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2018-01-04 18:24:28 -0200

    Updated file -  implemented router index

--- _app/main.js
+++ _app/main.js
@@ -1,1 +1,15 @@
-angular.module('blog' , ['mainDirective','ngAnimate']);
+angular.module('blog' , ['mainDirective','ngAnimate','ngRoute']).config(function($routeProvider , $locationProvider){
+	
+	var theme = 'themes/blog/';
+	$locationProvider.html5Mode(true);
+	$routeProvider.when('/',{
+		templateUrl : theme + 'index.html',
+		controller : 'PostController'
+	});
+
+	$routeProvider.when('/post/add',{
+		templateUrl : theme + 'cadastro.html'
+	});
+
+	$routeProvider.otherwise({redirectTo : '/'});
+});
