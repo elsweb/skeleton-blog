7483a7c0186f83abfd5865a940a4d452b0a2130b
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2018-01-05 13:17:03 -0200

    Updated File - implemented new directive btnAlert

--- _app/directives/main-directive.js
+++ _app/directives/main-directive.js
@@ -7,4 +7,14 @@ angular.module('mainDirective', []).directive('mainPainel',function(){
 	ddo.transclude = true;
 	ddo.templateUrl = '_app/directives/inc/main-painel.html';
 	return ddo;
+})
+.directive('btnAlert' , function(){
+	var ddo = {};
+	ddo.restrict = "AE";
+	ddo.scope = {
+		nome : '@', //Para strings
+		acao : '&'  //Para expressões
+	};
+	ddo.template = '<button ng-click="acao(post)" class="btn btn-danger btn-block">{{nome}}</button>';
+	return ddo;
 });
