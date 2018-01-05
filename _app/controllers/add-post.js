42a0ecd70e9cfd3f24b0b1dc62cd043fcf577fee
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2018-01-05 16:44:33 -0200

    Updated File - implemented service resource

--- _app/controllers/add-post.js
+++ _app/controllers/add-post.js
@@ -1,19 +1,13 @@
-angular.module('blog').controller('addPost', function($scope, $http, $resource, $routeParams){
+angular.module('blog').controller('addPost', function($scope, $http, Create, $resource, $routeParams){
 	$scope.post = {};
 	$scope.msg = '';
 	var url = 'http://elsweb.servehttp.com:3030'
-	 /*
-    Servidor ainda não suporta o uso de apenas uma unica url para as actions,
-    porém o blog já vem com suporte para o uso da mesma no futuro. Altere o nome da variavel
-    para para fazer uso tem todo o script quando tiver o suporte. Dica aula 10 do Curso.
-    */
-
+	
 	var edit = $resource(url + '/post/update/:id', null,{
 		update : {
 			method : 'PUT'
 		}
 	});
-
 	/*Servidor ainda não da suporte ao RECOVER, porém manter estrutura para fins Futuros*/
 	if($routeParams.id){
 		$http.get(url + '/post/consulta/' + $routeParams.id)
@@ -25,7 +19,6 @@ angular.module('blog').controller('addPost', function($scope, $http, $resource, 
 			$scope.msg = 'Não foi possível Recuperar a Postagem, Contacte o Administrador';
 		})
 	}
-
 	$scope.submeter = function(){
 		/*Servidor ainda não da suporte ao UPDATE, porém manter estrutura para fins Futuros*/
 		if($scope.formPost.$valid){
@@ -36,12 +29,10 @@ angular.module('blog').controller('addPost', function($scope, $http, $resource, 
 					$scope.msg = 'Não foi Possível alterar ' + $scope.post.post_title;
 				})
 			}else{
-				$http.post(url + '/post/create' , $scope.post)
-				.success(function(){
+				Create.add($scope.post, function(){
 					$scope.post = {};
 					$scope.msg = 'Postagem Cadastrada Com Sucesso';
-				})
-				.error(function(error){
+				},function(){
 					$scope.msg = 'Não foi Posssível Cadastrar Postagem';
 					console.log(error);
 				});
--- _app/controllers/post.js
+++ _app/controllers/post.js
@@ -1,17 +1,10 @@
-angular.module('blog').controller('PostController', function($scope, $http, $resource){
+angular.module('blog').controller('PostController', function($scope, ListAll, Delete){
 	$scope.posts = [];
 	$scope.bfilter = '';
     $scope.msg = '';
-    var url = 'http://elsweb.servehttp.com:3030';
-    /*
-    Servidor ainda não suporta o uso de apenas uma unica url para as actions,
-    porém o blog já vem com suporte para o uso da mesma no futuro. Altere o nome da variavel
-    para para fazer uso tem todo o script quando tiver o suporte. Dica aula 10 do Curso.
-    */
     
-    /*Listar Postagens*/
-    var consul  = $resource(url + '/post/consulta');
-    consul.query(function(rtn){
+   /*Listar Postagens*/
+    ListAll.query(function(rtn){
         $scope.posts = rtn;
     },function(error){
         console.log(error);
@@ -20,8 +13,7 @@ angular.module('blog').controller('PostController', function($scope, $http, $res
     /*Remover Postagem*/
     /*Servidor ainda não da suporte ao DELETE, porém manter estrutura para fins Futuros*/
     $scope.remove = function(post){
-        var del  = $resource(url + '/post/remove/:id');
-        del.delete({id : post.post_id }, function(){
+        Delete.delete({id : post.post_id }, function(){
             var indicePost = $scope.posts.indexOf(post);
             $scope.posts.splice(indicePost, 1);
             $scope.msg = 'Postagem ' + post.post_title + ' removido com sucesso';
